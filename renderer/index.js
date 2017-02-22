e3047e46d3e65ccc5ce2a266e15e98d8de2d6ee4
utv <k9amata@gmail.com>
2017-02-21 16:11:29 -0800

    add vs code setting, page components

--- renderer/index.js
+++ renderer/index.js
@@ -1,1 +1,25 @@
-console.log('hahahahahaha')
+
+import fs from 'fs'
+import path from 'path'
+
+let links = document.querySelectorAll('.menu li a')
+for (let link of links) {
+  link.addEventListener('click', (event) => {
+    event.preventDefault()
+    let page = event.target.getAttribute('href')
+    const pathToLoad = path.resolve(__dirname, '../components', page)
+    let targetEle = document.getElementsByClassName('task')[0]
+    loadPage(pathToLoad, targetEle)
+  })
+}
+
+function loadPage (pathToLoad, targetEle) {
+  const pageEle = fs.readFileSync(pathToLoad)
+  // remove children
+  while (targetEle.firstChild) {
+    targetEle.removeChild(targetEle.firstChild)
+  }
+
+  // add new page elements
+  targetEle.innerHTML = pageEle
+}
