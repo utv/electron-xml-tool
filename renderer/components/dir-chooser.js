50678e65ba2214ef7035d037483ee0a22a419756
utv <k9amata@gmail.com>
2017-02-27 18:52:10 -0800

    create xml for dir

--- renderer/components/dir-chooser.js
+++ renderer/components/dir-chooser.js
@@ -1,14 +1,17 @@
 import electron from 'electron'
-import appState from '../../appState.js'
 import path from 'path'
 import { createFileSelect } from './select-file-type'
+import { createRootNode } from '../utils/builder.js'
+// import appState from '../../appState.js'
 
 export function createDirChooser() {
   document.getElementsByClassName('dir-chooser')[0].addEventListener('click', () => {
     electron.remote.dialog.showOpenDialog({ properties: ['openDirectory'] }, (filePaths) => {
+      if (filePaths === undefined) return
+
       document.getElementsByTagName('strong')[0].innerHTML = path.basename(filePaths[0])
-      // appState['dir'] = filePaths[0]
       createFileSelect(filePaths[0])
+      createRootNode(filePaths[0])
     })
   })
 }
--- renderer/utils/builder.js
+++ renderer/utils/builder.js
@@ -1,6 +1,12 @@
-// let fs = require('fs')
+let fs = require('fs')
+let path = require('path')
 
-function createRootNode(file = {}) {
+export function createRootNode(file = '') {
+  let dest = path.join(file, path.basename(file) + '.xml')
+  console.log(dest)
+
+  if (fs.existsSync(dest)) return
+
   let xmlbuilder = require('xmlbuilder')
   var root = xmlbuilder.create('Parser', { encoding: 'UTF-8' })
   root.att('version', '1.0')
@@ -17,8 +23,9 @@ function createRootNode(file = {}) {
     })
 
   root.end({ pretty: true })
+  fs.writeFileSync(dest, root.toString())
   console.log(root.toString())
 }
 
-createRootNode()
+// createRootNode()
 // console.log(root);
